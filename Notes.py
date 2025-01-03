f2de395d55500ee095921b99b2f00f08a8fee077
Luke Allen <Luke.allen@thameswater.co.uk>
2025-01-03 21:41:52 +0000

    Planning_project

---
+++ Notes.py
@@ -0,0 +1,55 @@
+# income
+'''
+- User defined input 
+- float to 2 dp
+'''
+
+# income tax
+'''
+Website: https://www.gov.uk/income-tax-rates
+- Percentage depends on income (after pension)
+- float to 2 dp 
+'''
+
+# national insurance
+'''
+Website: https://www.gov.uk/national-insurance-rates-letters
+- Percentage depends on income (after pension)
+- float to 2 dp 
+'''
+
+# pension
+'''
+- User defined input 
+- percentage
+- calculated before tax 
+'''
+
+# Student loan
+'''
+Website: https://www.gov.uk/repaying-your-student-loan/what-you-pay
+- Percentage depends on income (after pension)
+- float to 2 dp 
+- depends on loan plan
+'''
+# Bonus
+'''
+- No Tax relief
+- Doesn't add to pension 
+'''
+
+
+
+'''
+1) Calculates amount paid in tax
+2) Amount after tax
+3) amount added to pension
+4) pension amount when getting to retirement stage
+5) student loan vs amount paid off per year graph
+    a) interest on the loan
+    b) how many years till you pay it back
+    C) ask if they make any voluntary contribution 
+
+
+
+'''
