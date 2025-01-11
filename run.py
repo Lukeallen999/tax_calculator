22aa702237042a072d5b732e7e5ea23452537532
Luke Allen <Luke.allen@thameswater.co.uk>
2025-01-11 22:29:37 +0000

    Added tax table and a new tools file

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
+# Modules
+import utils.tools as tools
 
-salary = float(input('Yearly salary before deductions: '))
-pension_percent = float(input('Percentage of income paid to pension (%) :'))/100
-pension_personal = salary * pension_percent
+def main():
+    tax_table_df = tools.get_tax_table()
+    tax_table_df = tools.clean_tax_table(tax_table_df)
 
-print(f'salary = £{"{:.2f}".format(salary)} \npension_percent = {pension_percent * 100}% \npension_personal = £{pension_personal}')
-taxable_salary = salary - pension_personal
+    print(tax_table_df)
+
+    salary = float(input('Yearly salary before deductions: '))
+    pension_percent = float(input('Percentage of income paid to pension (%) :'))/100
+    pension_personal = salary * pension_percent
+
+    print(f'salary = £{"{:.2f}".format(salary)} \npension_percent = {pension_percent * 100}% \npension_personal = £{pension_personal}')
+    taxable_salary = salary - pension_personal
+    
+
+if __name__ == "__main__":
+    main()
